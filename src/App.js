c0dfa49ac6fcb1059874cd98de2cd880669cff29
ahmady016 <ahmady09@gmail.com>
2019-05-10 19:24:55 +0200

    make sliderLength comes from props

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import addExtensions from './extensions'
 import './app.css'
 
 addExtensions()
-let start, end, _newPos
-const sliderLength = 5
-const slides = Array.random({ count: sliderLength, stringOps: { len: 6, base: '0123456789ABCDF' } })
+let start, end, _newPos, slides
 
 const doSlide = (start, end, newPos, currentPos, setCurrentPos, setCurrentDir) => e => {
   setCurrentDir(newPos > currentPos ? end : start)
   setCurrentPos(newPos)
 }
 
-const moveSlider = (sliderDir, buttonDir, currentPos, setCurrentPos, setCurrentDir, start, end) => (e) => {
+const moveSlider = (sliderLength, sliderDir, buttonDir, currentPos, setCurrentPos, setCurrentDir, start, end) => (e) => {
   if ( (buttonDir === 'backward' && sliderDir === 'ltr') || (buttonDir === 'forward' && sliderDir === 'rtl') )
     _newPos = ((currentPos-1) < 0) ? sliderLength-1 : currentPos-1
   else if ( (buttonDir === 'forward' && sliderDir === 'ltr') || (buttonDir === 'backward' && sliderDir === 'rtl') )
@@ -25,6 +23,7 @@ const renderItems = (
   start,
   end,
   currentPos = 0,
+  sliderLength,
   setCurrentPos = v => v,
   setCurrentDir = v => v
 ) => {
@@ -50,11 +49,14 @@ const renderItems = (
   })
 }
 
-function App ({ dir = 'rtl' }) {
+function App ({ dir = 'rtl', sliderLength = 5 }) {
   start = (dir === 'rtl') ? 'right' : 'left'
   end = (dir === 'rtl') ? 'left' : 'right'
   const [currentPos, setCurrentPos] = React.useState(0)
   const [currentDir, setCurrentDir] = React.useState(start)
+  React.useMemo(() => {
+    slides = Array.random({ count: sliderLength, stringOps: { len: 6, base: '0123456789ABCDF' } })
+  }, [sliderLength])
   return (
     <div className='center' style={{ direction: dir }}>
       <div className='slider'>
@@ -67,7 +69,7 @@ function App ({ dir = 'rtl' }) {
               : `translateX(-${currentPos * (100 / sliderLength)}%)`
           }}
         >
-          {renderItems('slides', currentPos)}
+          {renderItems('slides',  start, end, currentPos, sliderLength)}
         </div>
         <div className='slider__dots'>
           <span
@@ -77,11 +79,11 @@ function App ({ dir = 'rtl' }) {
               [end]: `${(sliderLength - 1) * 2 - currentPos * 2}rem`
             }}
           />
-          {renderItems('dots', start, end, currentPos, setCurrentPos, setCurrentDir)}
+          {renderItems('dots', start, end, currentPos, sliderLength, setCurrentPos, setCurrentDir)}
         </div>
         <div className='slider-buttons' style={{ flexDirection: (dir === 'rtl')? 'row-reverse' : 'row' }}>
-          <i class='material-icons backward'  onClick={moveSlider(dir, 'backward', currentPos, setCurrentPos, setCurrentDir, start, end)}>arrow_back_ios</i>
-          <i class='material-icons forward'   onClick={moveSlider(dir, 'forward', currentPos, setCurrentPos, setCurrentDir, start, end)}>arrow_forward_ios</i>
+          <i class='material-icons backward'  onClick={moveSlider(sliderLength, dir, 'backward', currentPos, setCurrentPos, setCurrentDir, start, end)}>arrow_back_ios</i>
+          <i class='material-icons forward'   onClick={moveSlider(sliderLength, dir, 'forward', currentPos, setCurrentPos, setCurrentDir, start, end)}>arrow_forward_ios</i>
         </div>
       </div>
     </div>
